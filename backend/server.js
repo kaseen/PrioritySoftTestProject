400bd4fa334479588a7bd95e9e8ffe49a92ccd8a
kaseen <srbtrxtheinsane@gmail.com>
2024-03-23 13:08:05 +0100

    Added /products endpoint to server

--- backend/server.js
+++ backend/server.js
@@ -6,9 +6,10 @@ const { getCategories, navigatePageTo, listProducts } = require('./etsy');
 
 const app = express();
 
+app.use(express.json());
 app.use(cors());
 
-app.get('/categories', async (req, res) => {
+app.get('/categories', async (_, res) => {
     const browser = await puppeteer.launch({ headless: false });
     const page = await browser.newPage();
 
@@ -21,29 +22,19 @@ app.get('/categories', async (req, res) => {
     await browser.close();
 });
 
+app.post('/products', async (req, res) => {
+    const browser = await puppeteer.launch({ headless: false });
+    const page = await browser.newPage();
+
+    await navigatePageTo(page, req.body.categoryLink);
+    const products = await listProducts(page);
+
+    res.send(JSON.stringify(products));
+
+    await browser.close();
+});
+
 const PORT = 4000;
 app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`);
 });
-
-/*(async () => {
-    // Launch the browser
-    const browser = await puppeteer.launch({ headless: false });
-    
-    // Create a page
-    const page = await browser.newPage();
-
-    const url = 'https://www.etsy.com/';
-
-    await navigatePageTo(page, url);
-
-    const categories = await getCategories(page);
-    await navigatePageTo(page, categories[0].categoryLink);
-
-    const products = await listProducts(page);
-
-    console.log(products)
-
-    // Close the browser
-    await browser.close();
-})();*/
